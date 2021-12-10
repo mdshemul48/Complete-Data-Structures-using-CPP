ecde7b6d4198f6d68850214d2fc8c3c36af79227
MD. Shimul <mdshemul480@gmail.com>
2021-12-11 02:48:33 +0600

    adding shift method

---
+++ linkedList.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class LinkedList
+{
+private:
+    Node *head = NULL;
+    Node *tail = NULL;
+    void firstNode(Node *n)
+    {
+        this->head = n;
+        this->tail = n;
+    }
+
+public:
+    void print()
+    {
+        if (!this->head)
+        {
+            return;
+        }
+
+        Node *temp = this->head;
+        while (temp)
+        {
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+
+    void push(int data)
+    {
+        Node *n = new Node(data);
+
+        if (!this->head)
+        {
+            firstNode(n);
+            return;
+        }
+
+        this->tail->next = n;
+        this->tail = n;
+    }
+    void pop()
+    {
+        if (!this->head)
+        {
+            return;
+        }
+
+        Node *temp = this->head;
+
+        if (!temp->next)
+        {
+            this->head = NULL;
+            this->tail = NULL;
+            return;
+        }
+
+        while (temp->next->next)
+        {
+            temp = temp->next;
+        }
+        delete temp->next->next;
+        temp->next = NULL;
+    }
+    void unshift(int data)
+    {
+        Node *n = new Node(data);
+        if (!this->head)
+        {
+            firstNode(n);
+            return;
+        }
+
+        n->next = this->head;
+        this->head = n;
+    }
+    void shift()
+    {
+        if (!this->head)
+        {
+            return;
+        }
+
+        if (!this->head->next)
+        {
+            this->head = NULL;
+            this->tail = NULL;
+        }
+
+        Node *deletedNode = this->head;
+        this->head = this->head->next;
+        delete deletedNode;
+    }
+};
+
+int main()
+{
+
+    LinkedList arr;
+
+    arr.push(1);
+    arr.push(2);
+    arr.push(3);
+    arr.push(4);
+    arr.pop();
+    arr.pop();
+    arr.pop();
+    arr.pop();
+    arr.unshift(1);
+    arr.unshift(2);
+    arr.unshift(3);
+    arr.unshift(4);
+    arr.print();
+
+    cout << "DONE" << endl;
+    return 0;
+}
